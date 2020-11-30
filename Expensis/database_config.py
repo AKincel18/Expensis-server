b83f4c628a2b5042955bed03c6e384f27892e1b5
AKincel18 <akincel97@gmail.com>
2020-11-30 18:11:07 +0100

    added database configuration

---
+++ Expensis/database_config.py
@@ -0,0 +1,8 @@
+DATABASE_CONFIG = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'NAME',  # set database name
+    'USER': 'postgres',
+    'PASSWORD': 'PASSWORD',  # set database password
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
+}
--- Expensis/settings.py
+++ Expensis/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from Expensis.database_config import DATABASE_CONFIG
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -75,10 +74,7 @@ WSGI_APPLICATION = 'Expensis.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': DATABASE_CONFIG
 }
 
 
