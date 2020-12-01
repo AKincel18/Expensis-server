e38810abe023bf35d381ad4dadd5b646e4dd95f2
Piotr Kapski <kapski.p@gmail.com>
2020-12-01 19:51:42 +0100

    Changed project structure.

--- MainExpensis/asgi.py
+++ MainExpensis/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Expensis project.
+ASGI config for MainExpensis project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Expensis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MainExpensis.settings')
 
 application = get_asgi_application()
--- MainExpensis/urls.py
+++ MainExpensis/urls.py
@@ -1,4 +1,4 @@
-"""Expensis URL Configuration
+"""MainExpensis URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
