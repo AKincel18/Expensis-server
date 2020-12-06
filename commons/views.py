f85bfcabf4af5d1a1ff11f575798ca6c17ee14d5
AKincel18 <akincel97@gmail.com>
2020-12-06 17:00:17 +0100

    added description to endpoints

--- commons/views.py
+++ commons/views.py
@@ -5,7 +5,11 @@ from commons.models import IncomeRange, AgeRange, Category
 from commons.serializers import RangeSerializer, CategorySerializer
 
 
+# path: /incomeRanges/
 class GetIncomeRanges(APIView):
+    """
+    get all income ranges
+    """
     def get(self, request):
         incomeRanges = IncomeRange.objects.all()
         RangeSerializer.Meta.model = IncomeRange
@@ -13,7 +17,11 @@ class GetIncomeRanges(APIView):
         return Response(serializer.data)
 
 
+# path: /ageRanges/
 class GetAgeRanges(APIView):
+    """
+    get all age ranges
+    """
     def get(self, request):
         ageRanges = AgeRange.objects.all()
         RangeSerializer.Meta.model = AgeRange
@@ -21,7 +29,11 @@ class GetAgeRanges(APIView):
         return Response(serializer.data)
 
 
+# path: /categories/
 class GetCategories(APIView):
+    """
+    get all categories
+    """
     def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
