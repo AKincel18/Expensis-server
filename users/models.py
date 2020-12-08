cdf6fb514e55d58fe403adcbcb024d5b248483e3
Kyeiv <krzy.poloczek@gmail.com>
2020-12-08 20:21:33 +0100

    Added JWT authentication

--- users/models.py
+++ users/models.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinLengthValidator
 from django.db import models
+from django.utils import timezone
 
 
-class User(models.Model):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
     password = models.CharField(validators=[MinLengthValidator(5)], max_length=250)
     gender = models.CharField(max_length=1)  # 'F' -> female, 'M' -> male
-    birth_date = models.DateField()
+    birth_date = models.DateField(default=timezone.now())
     monthly_limit = models.FloatField(null=True, blank=True, default=None)
     income_range = models.ForeignKey(
         'commons.IncomeRange',
