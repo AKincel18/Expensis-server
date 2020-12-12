d5e68f9fa6ee68d4e0b18b74d7c23b959222e284
Kyeiv <krzy.poloczek@gmail.com>
2020-12-12 16:44:58 +0100

    Optimized imports

--- users/views.py
+++ users/views.py
@@ -1,11 +1,7 @@
-from django.contrib.auth import get_user_model
-from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import exceptions
 
 from users.serializers import UserSerializerPost, UserSerializerGet, UserSerializerPut
 from users.service import get_user_by_id
